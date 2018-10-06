c9424575a472146c2b62ee2be0f219b940af4196
Kairat Yussupov <kairat_bmstu@mail.ru>
2018-10-06 22:29:31 +0600

    Update HelloBean.java
    deleted unnessary comments

--- src/main/java/com/mycompany/jsf/glassfish/example/HelloBean.java
+++ src/main/java/com/mycompany/jsf/glassfish/example/HelloBean.java
@@ -1,26 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.jsf.glassfish.example;
 
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
 
-/**
- *
- * @author Kairat Yussupov
- */
+
 @Named(value = "helloBean")
 @Dependent
 public class HelloBean {
 
     String message = "Hello World!";
     
-    /**
-     * Creates a new instance of HelloBean
-     */
     public HelloBean() {
     }
 
